b3fcf02dd9838a57874c45113e91c3ecf5afb84e
suku <adarshpsukumaran@gmail.com>
2024-03-21 21:34:23 +0530

    hey

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import  LoginSignup  from './Components/Login_signup/login_signup.jsx'
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
